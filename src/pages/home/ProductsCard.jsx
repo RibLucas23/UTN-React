fc7f3051fa2ceee6a176def1518370409c1cde04
RibLucas23 <92865990+RibLucas23@users.noreply.github.com>
2025-07-05 12:42:17 -0300

    hotFix: home products title

--- src/pages/home/ProductsCard.jsx
+++ src/pages/home/ProductsCard.jsx
@@ -11,7 +11,7 @@ export default function ProductsCard({ productData }) {
             />
          </figure>
          <div className="card-body">
-            <h2 className="card-title">{productData.name}</h2>
+            <h2 className="card-title">{productData.title}</h2>
             <p className='line-clamp-1  '>{productData.description}</p>
             <div className="card-actions justify-end items-center">
                <span className='text-center text-2xl font-bold'>${productData.price}</span>
